4841139d28344d3b2d18aa8d957e2fff1c195dc8
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-04 10:26:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// write code here
+function updateTask(id, newName){
+const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+  for(i = 0 ; i < tasks.length ; i++){
+    if(tasks[i].id == id){
+    tasks[i].name = newName
+      break
+    }
+  }
+  console.log(tasks)
+}
+updateTask(2, 'Travelling');
+
+// [
+//   { id: 1, name: 'Fishing' },
+//   { id: 2, name: 'Travelling' },
+//   { id: 3, name: 'Swimming' }
+// ]
